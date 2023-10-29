8df895acbf1ba6f49e8de076b9cf79a1dc73175c
S1D007 <s1d.official011001011@gmail.com>
2023-10-29 19:47:12 +0530

    Testting

--- src/pages/camera.tsx
+++ src/pages/camera.tsx
@@ -39,7 +39,7 @@ const camera = () => {
     navigator.mediaDevices
       .getUserMedia({
         video: {
-          aspectRatio: 10/7,
+          aspectRatio: 10/6,
           sampleSize: 10,
         },
       })
